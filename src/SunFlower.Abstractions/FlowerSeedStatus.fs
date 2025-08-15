4119b95381cf2fe4d0999755841c31850f4e5196
CoffeeLake <45635607+AlexeyTolstopyatov@users.noreply.github.com>
2025-08-15 22:50:33 +0700

    Core-Abstractions autoproperty fixed

--- src/SunFlower.Abstractions/FlowerSeedStatus.fs
+++ src/SunFlower.Abstractions/FlowerSeedStatus.fs
@@ -18,28 +18,33 @@ open SunFlower.Abstractions.Types
 /// Special flags i.e. <c>IsEnabled</c> switch
 /// in plugin's body for next usage (or not).
 /// </summary>
-type FlowerSeedStatus() as status = class
+type FlowerSeedStatus() = class
     inherit MarshalByRefObject()
     let mutable isEnabled : Boolean = false
     let mutable lastError : Exception = null
+    let mutable results = List<FlowerSeedResult>()
+    let mutable resultsExists : Boolean = results.Count <> 0
     /// <summary>
     /// Holds on state of seed's usage.
     /// Seeds HashSet contains all plugins (enabled and don't)
     /// but you can disable anybody "by hand"
     /// </summary>
-    member public f.IsEnabled
+    member public _.IsEnabled
         with get () = isEnabled
         and set flag = isEnabled <- flag
     /// <summary>
     /// Results list of external DLL
     /// </summary>
-    member val public Results : List<FlowerSeedResult> = List<FlowerSeedResult>()
+    member public _.Results
+        with get () = results
+        and set r = results <- r
     /// <summary>
     /// If result table is null or
     /// Count of results is zero -> connection failed
     /// returns false. 
     /// </summary>
-    member val public IsResultExists : Boolean = status.Results.Count <> 0
+    member public _.IsResultExists
+        with get () = resultsExists
     /// <summary>
     /// Stores last exception or exceptions chain
     /// </summary>
